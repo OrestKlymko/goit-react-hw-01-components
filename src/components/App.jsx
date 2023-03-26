03852bddce7b11d56922f8b1870bfae953c99330
OrestKlymko <orest.klymko2020@gmail.com>
2023-03-26 14:19:56 +0200

    react-create

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,19 @@
+import {Profile} from "../components/profile/profile"
+import { Statistic } from "./statistic/statistic";
+import { FriendList } from "./friends/friendsList";
+import { TransactionHistory } from "./transaction/transaction";
+///JSON UPLOAD///
+import user from "../components/json/user.json"
+import data from "../components/json/data.json"
+import friends from "../components/json/friends.json"
+import transactions from "../components/json/transactions.json"
+///Css///
+
+import cssFriend from "../components/friends/friendsList.module.css"
+
 export const App = () => {
   return (
+
     <div
       style={{
         height: '100vh',
@@ -7,10 +21,17 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Profile avatar={user.avatar} location={user.location} stats = {user.stats} tag={user.tag} username={user.username}/>
+      <Statistic title="Upload stats" data={data}/>
+      <ul className="friend-list">
+      {
+        friends.map(({avatar,name,isOnline,id}) => <li className={cssFriend.list} key={id}><FriendList avatar={avatar} name={name} isOnline={isOnline} /></li>)
+      }
+      </ul>
+      <TransactionHistory items={transactions}/>
     </div>
   );
 };
